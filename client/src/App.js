5bb3dc8e86c55957a50cb5bfc239d6fbe24227c5
Clint Brodar <clintbrodar@gmail.com>
2020-03-22 13:13:05 -0700

    Header

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,20 @@ function App() {
     <div className="App">
       <Navbar />
       <Container fluid>
-
+        <Row>
+          <div className="quarter-col">
+            <div id="large-logo-section">
+              <div className="large-logo">
+                H
+              </div>
+            </div>
+          </div>
+          <div className="three-quarters-col">
+            <div className="main-image-section">
+              <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80" alt="nature" className="main-image"/>
+            </div>
+          </div>
+        </Row>
       </Container>
     </div>
   );
