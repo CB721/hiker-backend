9238f9c46a83eb696f609223621d0bd7c44a82b4
Clint Brodar <clintbrodar@gmail.com>
2020-04-03 21:44:23 -0700

    proxy

--- client/src/utilities/api.js
+++ client/src/utilities/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     getHikes: function(location, miles, difficulty) {
-        return axios.get(`https://hiker-cb.herokuapp.com/api/hikes/${location}/${miles}/${difficulty}`);
+        return axios.get(`/api/hikes/${location}/${miles}/${difficulty}`);
     }
 }
